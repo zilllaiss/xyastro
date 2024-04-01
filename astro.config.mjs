2c9dc116898a5065111a3477a4d1c739bfedf048
zilllaiss <dzmkiza17@gmail.com>
2024-04-01 14:27:11 +0700

    added form and count

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import svelte from "@astrojs/svelte";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  output: "hybrid",
+  adapter: cloudflare()
 });
